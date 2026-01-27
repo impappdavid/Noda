b9bd8f2209a301d79861e33b19352e5eb1185e6c
Papp Dávid <impappdavid@gmail.com>
2026-01-27 14:10:16 +0100

    Creating the project with shadcn

---
+++ noda-frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './components/myComponents/LandingPage/LandingPage'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+
+        <Route path="/" element={<LandingPage />} />
+
+        
+      </Routes>
+    </>
+  )
+}
+
+export default App
