f512c474a4cd2bd23c96b4d7abd29797281c4f3e
Papp Dávid <impappdavid@gmail.com>
2026-03-26 18:40:59 +0100

    Refactoring thhe interactive tabs

--- noda-frontend/src/App.tsx
+++ noda-frontend/src/App.tsx
@@ -1,13 +1,6 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Overview from './components/myComponents/LandingPage/Overview/Overview'
-import HowItWorks from './components/myComponents/LandingPage/HowItWorks/HowItWorks'
-import ForRecruiters from './components/myComponents/LandingPage/ForRecruiters/ForRecruiters'
-import Features from './components/myComponents/LandingPage/Features/Features'
-import Pricing from './components/myComponents/LandingPage/Pricing/Pricing'
-import ChangeLog from './components/myComponents/LandingPage/Changelog/Changelog'
-import FAQ from './components/myComponents/LandingPage/Faq/Faq'
-import Waitlist from './components/myComponents/LandingPage/Waitlist/WaitList'
 import AppHome from './components/myComponents/App/Home/Home'
 import AppJobs from './components/myComponents/App/Jobs/Jobs'
 import AppCompanies from './components/myComponents/App/Companies/Companies'
@@ -50,13 +43,7 @@ function App() {
       <Routes>
 
         <Route path="/" element={<Overview />} />
-        <Route path="/howitworks" element={<HowItWorks />} />
-        <Route path="/forrecruiters" element={<ForRecruiters />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/changelog" element={<ChangeLog />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/waitlist" element={<Waitlist />} />
+      
 
 
         <Route path="/login" element={<AuthController />} />
