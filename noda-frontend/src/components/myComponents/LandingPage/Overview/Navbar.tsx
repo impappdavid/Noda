118907b7823b446bd26f984138227f3d6846cf3c
impappdavid <impappdavid@gmail.com>
2026-04-06 19:49:19 +0200

    Testing rounded design to the navbar

--- noda-frontend/src/components/myComponents/LandingPage/Overview/Navbar.tsx
+++ noda-frontend/src/components/myComponents/LandingPage/Overview/Navbar.tsx
@@ -1,11 +1,9 @@
 const Navbar = () => {
     return (
         <>
-            <nav className="flex items-center justify-between border-b border-zinc-300 bg-white shrink-0 sticky top-0 z-50">
+            <nav className="flex items-center justify-between border border-zinc-300 shrink-0 w-4xl top-2 z-50 p-2 fixed bg-zinc-100 rounded-lg shadow-2xl">
                 <div className="flex items-center h-full">
-                    <div className="p-2 text-[10px] h-full px-4 font-kodemono bg-orange-500 text-white tracking-[0.2em] font-medium">/NODA</div>
-
-                    
+                    <div className="p-2 text-xs h-full px-4 font-kodemono  text-black tracking-[0.2em] font-medium">/NODA</div>
                 </div>
 
                 {/* Right Auth Links */}
@@ -13,7 +11,7 @@ const Navbar = () => {
                     <a href="/login" className="text-[10px] p-2 px-4 border-l border-zinc-300 h-full flex items-center font-mono font-black text-zinc-600 hover:text-orange-600 uppercase tracking-widest transition-colors outline-none cursor-pointer">
                         Login
                     </a>
-                    <a href="/signup" className="h-full flex items-center px-4 bg-orange-500 text-white justify-center text-[10px] font-mono font-black uppercase tracking-widest hover:bg-orange-600 transition-colors outline-none cursor-pointer">
+                    <a href="/signup" className="h-full flex items-center px-4 rounded-md h-full bg-orange-500 text-white justify-center text-[10px] font-mono font-black uppercase tracking-widest hover:bg-orange-600 transition-colors outline-none cursor-pointer">
                         SignUp
                     </a>
                 </div>
