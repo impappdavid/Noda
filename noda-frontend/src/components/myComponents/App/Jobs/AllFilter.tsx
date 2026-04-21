c031fd197465965549636c119757300936bf6244
impappdavid <impappdavid@gmail.com>
2026-04-21 17:26:31 +0200

    Updating the jobs all filter ui

--- noda-frontend/src/components/myComponents/App/Jobs/AllFilter.tsx
+++ noda-frontend/src/components/myComponents/App/Jobs/AllFilter.tsx
@@ -1,5 +1,11 @@
 import { useState, KeyboardEvent } from "react";
-import { SlidersHorizontal, Check, Target, ChevronRight, X } from "lucide-react";
+import {
+  SlidersHorizontal,
+  Check,
+  Target,
+  ChevronRight,
+  X,
+} from "lucide-react";
 import {
   Dialog,
   DialogContent,
@@ -71,21 +77,21 @@ const AllFiltersDialog = () => {
         </DialogHeader>
 
         {/* SINGLE COLUMN GRID (STACKED) */}
-        <div className="p-4 grid grid-cols-1 gap-y-5 max-h-[75vh] overflow-y-auto scrollbar-hide">
+        <div className="grid grid-cols-1 max-h-[75vh] overflow-y-auto scrollbar-hide">
           {/* 01. SORTING PROTOCOL */}
           <section className="space-y-0 w-full">
-            <h4 className="text-[9px] font-mono font-black text-zinc-500 uppercase tracking-widest flex items-center gap-2 border-b border-zinc-100 pb-2">
+            <h4 className="text-[10px] p-2 w-full border-b border-zinc-300 font-mono font-black text-zinc-700 uppercase tracking-widest flex items-center gap-2 pb-2">
               01 // Sort By
             </h4>
-            <div className="flex bg-zinc-100 p-0.5 border border-zinc-200 w-full">
+            <div className="flex   border-b border-zinc-300 divide-x divide-zinc-300 w-full">
               {["recent", "relevance"].map((mode) => (
                 <button
                   key={mode}
                   onClick={() => setSortBy(mode)}
                   className={cn(
-                    "flex-1 py-2 text-[9px] font-black uppercase transition-all cursor-pointer",
+                    "flex-1 py-2 text-[9px] font-bold uppercase transition-all cursor-pointer",
                     sortBy === mode
-                      ? "bg-blue-500 text-white shadow-sm border border-zinc-300"
+                      ? "bg-blue-500 text-white shadow-sm "
                       : "text-zinc-500 hover:text-zinc-700",
                   )}
                 >
@@ -97,19 +103,19 @@ const AllFiltersDialog = () => {
 
           {/* 02. RESPOND TIME */}
           <section className="space-y-0">
-            <h4 className="text-[9px] font-mono font-black text-zinc-500 uppercase tracking-widest flex items-center gap-2 border-b border-zinc-100 pb-2">
-              02 // AVG response
+            <h4 className="text-[10px] p-2 border-b border-zinc-300 font-mono font-black text-zinc-700 uppercase tracking-widest flex items-center gap-2 ">
+              02 // AVG response time
             </h4>
-            <div className="grid grid-cols-4 gap-1 w-full">
+            <div className="grid grid-cols-4 w-full divide-x divide-zinc-300 border-b border-zinc-300">
               {RESPOND_TIMES.map((time) => (
                 <button
                   key={time}
                   onClick={() => setRespondTime(time)}
                   className={cn(
-                    "px-2 py-2 border text-[9px] font-bold uppercase transition-all cursor-pointer",
+                    "px-2 py-2 text-[9px] font-bold uppercase  transition-all cursor-pointer",
                     respondTime === time
-                      ? "bg-blue-500 text-white border-blue-500"
-                      : "bg-white text-zinc-500 border-zinc-200 hover:border-zinc-400",
+                      ? "bg-blue-500 text-white "
+                      : "bg-white text-zinc-500 ",
                   )}
                 >
                   {time}
@@ -120,58 +126,47 @@ const AllFiltersDialog = () => {
 
           {/* 03. JOB TYPE */}
           <section className="space-y-0 ">
-            <h4 className="text-[9px] font-mono font-black text-zinc-500 uppercase tracking-widest flex items-center gap-2 border-b border-zinc-100 pb-2">
-              03 // Contract_Type_Registry
+            <h4 className="text-[10px] font-mono p-2 border-b border-zinc-300 font-black text-zinc-700 uppercase tracking-widest flex items-center gap-2">
+              03 // Contract Type
             </h4>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-y-4 gap-x-6">
-              {JOB_TYPES.map((type) => (
-                <div
-                  key={type}
-                  onClick={() =>
-                    setSelectedTypes((prev) =>
-                      prev.includes(type)
-                        ? prev.filter((t) => t !== type)
-                        : [...prev, type],
-                    )
-                  }
-                  className="flex items-center gap-2 cursor-pointer group"
-                >
-                  <div
+            <div className="grid grid-cols-2 md:grid-cols-5 border border-zinc-300 divide-x divide-zinc-300 w-fit overflow-hidden">
+              {JOB_TYPES.map((type) => {
+                const isSelected = selectedTypes.includes(type);
+
+                return (
+                  <button
+                    key={type}
+                    type="button"
+                    onClick={() =>
+                      setSelectedTypes((prev) =>
+                        prev.includes(type)
+                          ? prev.filter((t) => t !== type)
+                          : [...prev, type],
+                      )
+                    }
                     className={cn(
-                      "w-4 h-4 border flex items-center justify-center transition-all",
-                      selectedTypes.includes(type)
-                        ? "bg-blue-500 border-blue-600 shadow-[0_0_8px_rgba(249,115,22,0.3)]"
-                        : "border-zinc-300 group-hover:border-zinc-500",
-                    )}
-                  >
-                    {selectedTypes.includes(type) && (
-                      <Check size={11} className="text-white" strokeWidth={4} />
-                    )}
-                  </div>
-                  <span
-                    className={cn(
-                      "text-[10px] uppercase font-bold tracking-tight",
-                      selectedTypes.includes(type)
-                        ? "text-zinc-900"
-                        : "text-zinc-500",
+                      "px-3 py-2 text-[9px] font-bold uppercase transition-all cursor-pointer outline-none",
+                      isSelected
+                        ? "bg-blue-500 text-white"
+                        : "bg-white text-zinc-500 hover:bg-zinc-50",
                     )}
                   >
                     {type}
-                  </span>
-                </div>
-              ))}
+                  </button>
+                );
+              })}
             </div>
           </section>
 
           {/* 04. SALARY */}
           <section className="space-y-0 ">
-            <h4 className="text-[9px] font-mono font-black text-zinc-500 uppercase tracking-widest flex items-center gap-2 border-b border-zinc-100 pb-2">
-              04 // Annual_Salary_Limit
+            <h4 className="text-[10px] p-2 border-b border-zinc-300 font-mono font-black text-zinc-700 uppercase tracking-widest flex items-center gap-2">
+              04 // Annual Salary Range
             </h4>
-            <div className="flex items-center gap-4 w-full">
+            <div className="flex items-center gap-4 w-full bg-blue-500">
               <div className="relative flex-1">
                 <span className="absolute left-2 top-1 text-[9px] font-mono text-zinc-500 uppercase tracking-tighter">
-                  Minimum_k
+                  Minimum USD
                 </span>
                 <input
                   type="number"
@@ -179,17 +174,17 @@ const AllFiltersDialog = () => {
                   onChange={(e) =>
                     setSalary({ ...salary, min: e.target.value })
                   }
-                  className="w-full pl-2 pr-6 pt-4 pb-1 border border-zinc-300 text-[12px] font-mono font-bold outline-none focus:border-blue-500 bg-zinc-50/50"
+                  className="w-full pl-2 pr-6 pt-4 pb-1 border border-zinc-300 text-[12px] font-mono font-bold outline-none focus:border-blue-500 bg-white"
                   placeholder="0"
                 />
                 <span className="absolute right-2 bottom-1.5 text-[11px] font-mono font-black text-zinc-900">
                   k
                 </span>
               </div>
-              <ChevronRight size={14} className="text-zinc-600 shrink-0" />
+              <ChevronRight size={14} className="text-white shrink-0" />
               <div className="relative flex-1">
                 <span className="absolute left-2 top-1 text-[9px] font-mono text-zinc-500 uppercase tracking-tighter">
-                  Maximum_k
+                  Maximum USD
                 </span>
                 <input
                   type="number"
@@ -197,7 +192,7 @@ const AllFiltersDialog = () => {
                   onChange={(e) =>
                     setSalary({ ...salary, max: e.target.value })
                   }
-                  className="w-full pl-2 pr-6 pt-4 pb-1 border border-zinc-300 text-[12px] font-mono font-bold outline-none focus:border-blue-500 bg-zinc-50/50"
+                  className="w-full pl-2 pr-6 pt-4 pb-1 border border-zinc-300 text-[12px] font-mono font-bold outline-none focus:border-blue-500 bg-white"
                   placeholder="250"
                 />
                 <span className="absolute right-2 bottom-1.5 text-[11px] font-mono font-black text-zinc-900">
@@ -209,7 +204,7 @@ const AllFiltersDialog = () => {
 
           {/* 05. SKILL REGISTRY */}
           <section className="space-y-0 ">
-            <h4 className="text-[9px] font-mono font-black text-zinc-500 uppercase tracking-widest border-b border-zinc-100 pb-2 flex items-center gap-2">
+            <h4 className="text-[10px] p-2 border-b border-zinc-300 font-mono font-black text-zinc-700 uppercase tracking-widest flex items-center gap-2">
               05 // Skills
             </h4>
 
@@ -220,24 +215,24 @@ const AllFiltersDialog = () => {
                 onChange={(e) => setSkillInput(e.target.value)}
                 onKeyDown={handleAddSkill}
                 placeholder="Press enter to add skills..."
-                className="w-full px-3 py-2 border border-zinc-300 bg-white outline-none text-[10px] font-mono font-bold uppercase placeholder:text-zinc-500"
+                className="w-full px-3 py-2 border-b border-zinc-300 bg-white outline-none text-[10px] font-mono font-bold uppercase placeholder:text-zinc-500"
               />
             </div>
 
-            <div className="flex flex-wrap gap-1.5 pt-2  ">
+            <div className="flex flex-wrap divide-x divide-zinc-300 pt-2  ">
               {skills.length > 0 ? (
                 skills.map((skill) => (
                   <div
                     key={skill}
                     onClick={() => removeSkill(skill)}
-                    className="flex items-center cursor-pointer gap-2 px-2 py-1 bg-blue-500 text-white text-[9px] font-bold uppercase tracking-widest transition-colors hover:bg-blue-600"
+                    className="flex items-center cursor-pointer gap-2 px-2 py-1 bg-blue-500 text-white text-[9px] font-semibold uppercase tracking-widest transition-colors hover:bg-blue-600"
                   >
                     {skill}
                   </div>
                 ))
               ) : (
-                <span className="text-[9px] font-mono text-zinc-300 uppercase italic">
-                  Buffer_Empty...
+                <span className="text-[9px] pl-2 py-1.5 font-mono text-zinc-600 uppercase italic">
+                  No Skills Selected...
                 </span>
               )}
             </div>
@@ -245,11 +240,11 @@ const AllFiltersDialog = () => {
         </div>
 
         {/* CONTROL FOOTER */}
-        <DialogFooter className="py-2 px-4 border-t border-zinc-300 flex items-center justify-between sm:justify-between w-full">
-          <button className="text-[11px] font-mono font-black text-zinc-500 uppercase hover:text-red-600 transition-colors cursor-pointer">
+        <DialogFooter className="border-t mt-2 border-zinc-300 gap-0 flex items-center justify-between sm:justify-between w-full">
+          <button className="text-[11px] w-1/2 bg-zinc-200/80 h-full  font-mono font-black text-zinc-500 uppercase hover:text-red-600 transition-colors cursor-pointer">
             REset filter
           </button>
-          <button className="rounded-none bg-zinc-900 hover:bg-blue-600 text-white text-[10px] font-black uppercase tracking-widest h-10 px-12 transition-all shadow-lg cursor-pointer active:scale-95">
+          <button className="rounded-none w-1/2 bg-blue-500 hover:bg-blue-600 text-white text-[10px] font-black uppercase tracking-widest h-10 px-12 transition-all shadow-lg cursor-pointer active:scale-95">
             Search
           </button>
         </DialogFooter>
